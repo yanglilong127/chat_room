14b04ca0d6992515161b495d686f481f3e0dba1a
burt <1105332245@qq.com>
2017-10-13 18:54:36 +0800

    增加将base64编码转为图片说明

--- server.js
+++ server.js
@@ -74,3 +74,20 @@ io.on('connection',(socket)=>{
 	});
 	
 });
+
+
+//后台接受的图片是base64编码字符串，要将其转换为图片
+/****
+var imgData = req.body.imgData; //接收前台POST过来的base64
+//过滤data:URL
+var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
+var dataBuffer = new Buffer(base64Data, 'base64');
+fs.writeFile("out.png", dataBuffer, function(err) {
+	if(err){
+	  res.send(err);
+	}else{
+	  res.send("保存成功！");
+	}
+});
+
+****/
